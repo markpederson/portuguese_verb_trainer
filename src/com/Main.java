8fbecf038ff4e68c7095a553af071c1d4aab84e3
markpederson <mark.pederson@lightspeehq.com>
2019-10-05 23:01:45 -0400

    hide cpp files

--- src/com/Main.java
+++ src/com/Main.java
@@ -1,8 +1,20 @@
 package com;
 
+import java.util.Random;
+
 public class Main {
     public static void main(String[] args) {
-        Verb falar = new Verb("falar");
-        falar.print_all();
+        String[] raw_verb_list = {"amar", "beber", "cantar", "chegar", "comprar", "estudar", "falar", "ficar",
+                                  "comer", "conhecer", "correr", "vender", "viver",
+                                  "dormir", "partir"};
+
+        Verb[] verb_list= new Verb[raw_verb_list.length];
+        for (int i = 0; i < raw_verb_list.length; i++) {
+            verb_list[i] = new Verb(raw_verb_list[i]);
+        }
+
+        for (Verb verb : verb_list) {
+            verb.print_all();
+        }
     }
 }
